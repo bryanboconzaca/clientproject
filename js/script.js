162612bab8cece0ea37882a341c16cafc710f6da
bryan boconzaca <bboconzaca2022@bklawtech.com>
2019-05-16 22:21:03 +0000

    added track titles

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+
+$("#answers").click(function(){
+      var genre = $("#question3").val()
+      
+      console.log(genre)
+      var musicUrl = "https://api.soundcloud.com/tracks?genres="+genre+"&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+      
+      $.ajax({
+         url:musicUrl,
+         method: "GET",
+         success: function(response){
+             console.log(response);
+             $(".questions").hide()
+             var tracks= $(".tracks")
+             response.forEach(function(track){
+                console.log(track); 
+                tracks.append("<p>"+track.title+"</p>")
+             })
+         },
+      });
+});
