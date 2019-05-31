08486cb0f126c054754623da4a6f2fa3b1fe866b
bryan boconzaca <bboconzaca2022@bklawtech.com>
2019-05-31 03:56:56 +0000

    bakcground colored

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ $("#answers").click(function(){
       var genre = $("#question3").val()
       var mood = $("#question4").val()
       var person = $("#question1").val()
-      var musicUrl = "https://api.soundcloud.com/tracks?genre="+genre+"&client_id=5aa8e389ba4e24b6106af5159ab3e344&tags="+mood+","+person;
+      var description = $("#question2").val()
+      var culture = $("#question5").val()
+      var musicUrl = "https://api.soundcloud.com/tracks?genre="+genre+"&client_id=5aa8e389ba4e24b6106af5159ab3e344&tags="+mood+","+person+","+description+","+culture;
       console.log(musicUrl)
       $.ajax({
          url:musicUrl,
